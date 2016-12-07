8c4cbc51640db4e4127d305f851cbc44bfa42500
Hoàng Phát Phạm <Hong Pht Phm>
2016-12-07 10:56:12 +0700

    Add folder FormAjax

---
+++ FormAjax/demo/index.php
@@ -0,0 +1,13 @@
+﻿
+    <?php    
+        if (isset($_POST['user'])){
+            $user = $_POST['user'];
+            if ($user == "ngochoang3"){
+                echo "true";
+            }	
+            else{
+                echo "false";
+            }   
+        }
+    ?>
+    
